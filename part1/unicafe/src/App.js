be959d9b4fe8d2684f615a8d04bb8f7aef1a4fc5
Frederik Röckle <frederik.roeckle@gmx.de>
2021-11-24 23:54:29 +0100

    1.9: unicafe step4

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -36,6 +36,15 @@ const Statistics = (prop) => {
     return (good / all()) * 100;
   }
 
+  if (all() == 0) {
+    return (
+      <div>
+        <h1>statistics</h1>
+        <p>No feedback given</p>
+      </div>
+    );
+  }
+
   return (
     <div>
       <h1>statistics</h1>
