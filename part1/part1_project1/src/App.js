a78623a2d3bb7a9fa11fb3cebcb59589632466b8
Frederik Röckle <frederik.roeckle@gmx.de>
2021-11-24 21:20:17 +0100

    course information, step1

---
+++ part1/part1_project1/src/App.js
@@ -0,0 +1,49 @@
+import React from "react";
+
+const App = () => {
+  const course = "Half Stack application development";
+  const part1 = "Fundamentals of React";
+  const exercises1 = 10;
+  const part2 = "Using props to pass data";
+  const exercises2 = 7;
+  const part3 = "State of a component";
+  const exercises3 = 14;
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content part={part1} excersises={exercises1} />
+      <Content part={part2} excersises={exercises2} />
+      <Content part={part3} excersises={exercises3} />
+      <Total total={exercises1 + exercises2 + exercises3} />
+    </div>
+  );
+};
+
+const Header = (props) => {
+  return (
+    <div>
+      <h1>{props.course}</h1>
+    </div>
+  );
+};
+
+const Content = (props) => {
+  return (
+    <div>
+      <p>
+        {props.part} {props.excersises}
+      </p>
+    </div>
+  );
+};
+
+const Total = (props) => {
+  return (
+    <div>
+      <p>Number of excersises {props.total}</p>
+    </div>
+  );
+};
+
+export default App;
